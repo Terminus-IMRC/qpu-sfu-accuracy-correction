61e883533834eb39062a0c0fadcd50a41abc9895
Terminus <i.can.speak.c.and.basic@gmail.com>
2017-05-30 13:31:16 +0900

    Minor changes on output formatting

--- main.py
+++ main.py
@@ -5,14 +5,30 @@ import numpy as np
 from videocore.assembler import qpu
 from videocore.driver import Driver
 
+def run_code(code, X):
+    with Driver() as drv:
+        X = drv.copy(np.array(X, dtype = 'float32'))
+        Y = drv.alloc((16), dtype = 'float32')
+        drv.execute(
+                n_threads = 1,
+                program = drv.program(boilerplate, code),
+                uniforms = [X.address, Y.address])
+        return np.copy(Y)
+
 def float_as_uint(f):
     return struct.unpack('!I', struct.pack('!f', f))[0]
 
 def uint_as_float(i):
     return struct.unpack('!f', struct.pack('!I', i))[0]
 
-def uint_as_hexstr(i):
-    return "%08x" % i
+def print_uint(prefix, v):
+    print(prefix, list(map(lambda x: "%08x" % x, v)))
+
+def print_float(prefix, v):
+    print(prefix, list(map(lambda x: "%.4e" % x, v)))
+
+def print_float_as_uint(prefix, v):
+    print_uint(prefix, list(map(float_as_uint, v)))
 
 @qpu
 def boilerplate(asm, f):
@@ -81,37 +97,30 @@ def code_sfu_recip_3(asm):
     fsub(r2, 2.0, r2)
     fmul(vpm, r1, r2)
 
-def run_code(code, X):
-    with Driver() as drv:
-        X = drv.copy(np.array(X, dtype = 'float32'))
-        Y = drv.alloc((16), dtype = 'float32')
-        drv.execute(
-                n_threads = 1,
-                program = drv.program(boilerplate, code),
-                uniforms = [X.address, Y.address])
-        return np.copy(Y)
-
-def do_recip():
+def do_recip(xf):
     print('# recip')
+
+    yf0 = run_code(code_sfu_recip_0, xf)
+    yf1 = run_code(code_sfu_recip_1, xf)
+    yf2 = run_code(code_sfu_recip_2, xf)
+    yf3 = run_code(code_sfu_recip_3, xf)
+    yf_ref = map(lambda x: np.float32(1.0) / np.float32(x), xf)
+
+    print_float_as_uint('output 0:  ', yf0)
+    print_float_as_uint('output 1:  ', yf1)
+    print_float_as_uint('output 2:  ', yf2)
+    print_float_as_uint('output 3:  ', yf3)
+    print_float_as_uint('output ref:', yf_ref)
+
+def main():
     xi = np.random.uniform(1 << 23, (255 << 23), 16).astype(np.uint32)
     xf = list(map(uint_as_float, xi))
-    yf0 = run_code(code_sfu_recip_0, xf)
-    yi0 = list(map(float_as_uint, yf))
-    yf1 = run_code(code_sfu_recip_1, xf)
-    yi1 = list(map(float_as_uint, yf1))
-    yf2 = run_code(code_sfu_recip_2, xf)
-    yi2 = list(map(float_as_uint, yf2))
-    yf3 = run_code(code_sfu_recip_3, xf)
-    yi3 = list(map(float_as_uint, yf3))
-    yf_ref = list(map(lambda x: np.float32(1.0) / np.float32(x), xf))
-    yi_ref = list(map(float_as_uint, yf_ref))
-    print("input aslist(map(uint_as_hexstr, xi)))
-    print(list(map(lambda x: "%.4e" % x, xf)))
-    print(list(map(uint_as_hexstr, yi0)))
-    print(list(map(uint_as_hexstr, yi1)))
-    print(list(map(uint_as_hexstr, yi2)))
-    print(list(map(uint_as_hexstr, yi3)))
-    print(list(map(uint_as_hexstr, yi_ref)))
+    print_uint('input:', xi)
+    print_float('input as float:', xf)
+    print()
+
+    do_recip(xf)
+    print()
 
 if __name__ == '__main__':
     main()
